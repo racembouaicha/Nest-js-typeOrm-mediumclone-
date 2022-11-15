682f6721296371600d04ce7ef37cc4ffb3e12445
racem <racembouaicha123@gmail.com>
2022-11-15 23:06:27 +0100

    create user  entity and signin api

---
+++ src/Entity/User.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from "typeorm"
+import {hash} from 'bcrypt';
+
+@Entity({name: 'users'})
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    email: string
+
+    @Column()
+    username: string
+
+
+    @Column({default: ''})
+    bio: string
+
+    @Column({ default: '' })
+    image: string
+
+    @Column()
+    password: string
+
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await hash(this.password, 10);
+    }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { UserEntity } from '@app/Entity/User.entity';
+import { Controller, Post, Body, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('auth')
+export class UserController {
+    constructor(private readonly userService: UserService){}
+   
+    @Post('users')
+    @UsePipes(new ValidationPipe())
+    async createUser(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
+        const user = await this.userService.createUser(createUserDto);
+        return this.userService.buildUserResponse(user)
+    }
+}
